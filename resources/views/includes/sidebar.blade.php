011c12d56eb6d254fce4e233cabcab445e5fbd68
Shibly <shibly.phy@gmail.com>
2025-02-14 05:39:17 +0600

    category icon updated on sidebar

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -211,8 +211,8 @@
 	<!-- Download SVG icon from http://tabler.io/icons/icon/home -->
 	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
          stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1"><path
-                d="M5 12l-2 0l9 -9l9 9l-2 0"/><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"/><path
-                d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"/></svg>
+            d="M5 12l-2 0l9 -9l9 9l-2 0"/><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"/><path
+            d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"/></svg>
 </span>
                         <span class="nav-link-title">
 					Home
@@ -224,10 +224,12 @@
                     <a class="nav-link" href="{{route('categories.index')}}">
 				<span class="nav-link-icon d-md-none d-lg-inline-block">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                         stroke="currentColor"
-                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1"><path
-                            d="M9 11l3 3l8 -8"/><path
-                            d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9"/></svg>
+                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                         class="icon icon-tabler icons-tabler-outline icon-tabler-category"><path stroke="none"
+                                                                                                  d="M0 0h24v24H0z"
+                                                                                                  fill="none"/><path
+                            d="M4 4h6v6h-6z"/><path d="M14 4h6v6h-6z"/><path d="M4 14h6v6h-6z"/><path
+                            d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"/></svg>
                 </span>
                         <span class="nav-link-title">
 					Categories
