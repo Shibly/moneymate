8289cc00f37b9c1d79c82bd66a06d043db44e805
Shibly <shibly.phy@gmail.com>
2025-02-28 02:01:35 +0600

    Footer updated

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,24 +3,11 @@
         <div class="row text-center align-items-center flex-row-reverse">
             <div class="col-lg-auto ms-lg-auto">
                 <ul class="list-inline list-inline-dots mb-0">
-                    <li class="list-inline-item"><a href="https://tabler.io/docs" target="_blank"
-                                                    class="link-secondary" rel="noopener">Documentation</a></li>
-                    <li class="list-inline-item"><a href="./license.html" class="link-secondary">License</a>
+                    <li class="list-inline-item"><x-tabler-phone/>{{get_option('phone')}}</li>
                     </li>
-                    <li class="list-inline-item"><a href="https://github.com/tabler/tabler" target="_blank"
-                                                    class="link-secondary" rel="noopener">Source code</a></li>
+
                     <li class="list-inline-item">
-                        <a href="https://github.com/sponsors/codecalm" target="_blank" class="link-secondary"
-                           rel="noopener">
-                            <!-- Download SVG icon from http://tabler.io/icons/icon/heart -->
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                 stroke-linejoin="round" class="icon text-pink icon-inline icon-4">
-                                <path
-                                    d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"/>
-                            </svg>
-                            Sponsor
-                        </a>
+                        <x-tabler-home-link/> {{get_option('address')}}
                     </li>
                 </ul>
             </div>
@@ -28,13 +15,7 @@
                 <ul class="list-inline list-inline-dots mb-0">
                     <li class="list-inline-item">
                         Copyright &copy; 2025
-                        <a href="." class="link-secondary">Tabler</a>.
-                        All rights reserved.
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="./changelog.html" class="link-secondary" rel="noopener">
-                            v1.0.0
-                        </a>
+                        <a href="{{get_option('web_site')}}" class="link-secondary">{{get_option('company_name')}}</a>
                     </li>
                 </ul>
             </div>
